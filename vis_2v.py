30c6b58bb15a7ab5007d53d149b206da2d14591d
澁田謙太 <kenta@kentanoMacBook-Air-2.local>
2020-01-11 10:56:41 +0900

    update

--- vis_2v.py
+++ vis_2v.py
@@ -1,6 +1,19 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors
+import numpy as np; np.random.seed(0)
+import pandas as pd
 
+
+import matplotlib.font_manager as fon
+del fon.weight_dict['roman']
+matplotlib.font_manager._rebuild()
+
+plt.rcParams['font.family'] = 'Times New Roman' #全体のフォントを設定
+plt.rcParams["mathtext.fontset"] = "stix" # これを入れておくと，斜体にするときれいになる．
+# 軸を内側に入れる
+plt.rcParams['xtick.direction'] = 'in' # x axis in
+plt.rcParams['ytick.direction'] = 'in' # y axis in
+plt.rcParams["font.size"] = 14
 nc = 30
 rl1 = 10
 rl2 = 20
@@ -53,15 +66,24 @@ np_interface_y = np.loadtxt(f_name_interface,
                           skiprows=0,
                           usecols=(1)
                           )
-
+plt.xlabel('$\it{x}$'+' [m]')
+plt.ylabel('$\it{y}$'+' [m]')
 plt.quiver(np_crd_x,np_crd_y,np_v_x,np_v_y,angles='xy',scale_units='xy',scale=None, width=0.005)
+'''
 for i in range(4*nc):
     if i == 4*nc - 1:
-        plt.plot([np_interface_x[int(i+interface_start)], np_interface_x[int(interface_start)]], [np_interface_y[int(i+interface_start)], np_interface_y[int(interface_start)]],'m-.', lw=1.0)
+        plt.plot([np_interface_x[int(i+interface_start)], np_interface_x[int(interface_start)]], [np_interface_y[int(i+interface_start)], np_interface_y[int(interface_start)]],'m-.', lw=1.0, label = "Interface")
     else:
         plt.plot([np_interface_x[int(i+interface_start)], np_interface_x[int(i+1+interface_start)]], [np_interface_y[int(i+interface_start)], np_interface_y[int(i+1+interface_start)]],'m-.', lw=1.0)
-
+'''
+for i in range(4*nc):
+    if i == 4*nc - 1:
+        plt.plot([np_interface_x[i], np_interface_x[0]], [np_interface_y[i], np_interface_y[0]],'m-.', lw=1.0, label = "Interface")
+    else:
+        plt.plot([np_interface_x[i], np_interface_x[i+1]], [np_interface_y[i], np_interface_y[i+1]],'m-.', lw=1.0)
+plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0.1,labelspacing=0.1,fontsize=14)
+plt.tight_layout()
 plt.axes().set_aspect('equal')
-#plt.savefig("sample.ps",format = 'ps')
-plt.savefig("sample.png",format = 'png', dpi=1000)
+plt.savefig("sample.svg",format = 'svg')
+#plt.savefig("sample.png",format = 'png', dpi=1000)
 plt.show()
