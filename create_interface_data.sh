aea81d6f5e7ca86af713d6aa7579101198cbc811
Kenta Shibuta <kenta@Kenta-no-iMac.local>
2021-07-21 10:38:22 +0900

    update

--- create_interface_data.sh
+++ create_interface_data.sh
@@ -2,6 +2,6 @@
 echo ファイルのパスを入力:
 read input_file_name
 output_file_name=`date +"%Y%m%d%H%M%S"`
-f_name_crd='_crd'
+f_name_crd='_interface'
 f_name_v='_v'
 awk '/# interface_start/,/# interface_end/' $input_file_name|awk '! /^#/'|awk 'BEGIN{OFS=","} {print $2, $3}'>$output_file_name$f_name_crd.csv
